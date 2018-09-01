85edbe06cab73377a9dfed030daad59c7f18f474
Flaviano Marinho <flauberjp@gmail.com>
2018-09-01 20:24:12 -0300

    Accessing remote repository from Heroku

--- project.py
+++ project.py
@@ -6,20 +6,30 @@ from urllib.parse import unquote, parse_qs
 import os
 import threading
 from socketserver import ThreadingMixIn
-#import pygit2
+import pygit2
 import os
 from time import gmtime, strftime
 
 my_user = 'uninitialized'
 my_password = 'uninitialized'
 
-#repository_url = "https://github.com/flauberjp/MovieTrailerWebsite"
-#local_repository_name = repository_url.rsplit('/', 1)[-1]
-#file_of_evidences = local_repository_name + '/index.html'
+repository_url = "https://github.com/flauberjp/MovieTrailerWebsite"
+local_repository_name = repository_url.rsplit('/', 1)[-1]
+file_of_evidences = local_repository_name + '/index2.html'
 
-#repository = ''
-#if (os.path.exists(local_repository_name) == False):
-#    repository = pygit2.clone_repository(repository_url, local_repository_name)
+repository = ''
+if (os.path.exists(local_repository_name) == False):
+    repository = pygit2.clone_repository(repository_url, local_repository_name)
+
+if(os.path.exists(file_of_evidences) == False):
+    with open(file_of_evidences, 'w'): 
+        pass
+
+message = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+with open(file_of_evidences, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(message + '<BR>' + content)
 
 memory = {}
 
