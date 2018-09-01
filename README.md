d91bea416ea0359916523b92f2e7e2d2e7987190
Flaviano Marinho <flauberjp@gmail.com>
2018-09-01 06:52:20 -0300

    Added Python program heroku template It can be deployed to Heroku

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# flauberjp-contributions-cal
+# contributionsCalendar
+
+
+
+Step-by-step:
+1. Go to Heroku, authenticate there, then on your Dashboard, click _New_ button, then select the option _Create new app_
+2. Set the App name, for instace, to <youUserName>-contributions-cal, in my case flauberjp-contributions-cal, click _Create app_ button
+
+----------------------
+
+Steps to deployment
+Here's an overview of the steps you'll need to complete. We'll be going over each one in more detail.
+
+. Check your server code into a new local Git repository.
+. Sign up for a free Heroku account.
+. Download the Heroku command-line interface (CLI).
+. Authenticate the Heroku CLI with your account: heroku login
+. Create configuration files Procfile, requirements.txt, and runtime.txt and check them into your Git repository.
+. Modify your server to listen on a configurable port.
+. Create your Heroku app: heroku create your-app-name
+. Push your code to Heroku with Git: git push heroku master
+
+
---
+++ project.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import http.server
+import requests
+from urllib.parse import unquote, parse_qs
+import os
+import threading
+from socketserver import ThreadingMixIn
+
+memory = {}
+
+form = '''<!DOCTYPE html>
+<title>Bookmark Server</title>
+<form method="POST">
+    <label>Long URI:
+        <input name="longuri">
+    </label>
+    <br>
+    <label>Short name:
+        <input name="shortname">
+    </label>
+    <br>
+    <button type="submit">Save it!</button>
+</form>
+<p>URIs I know about:
+<pre>
+{}
+</pre>
+'''
+
+
+def CheckURI(uri, timeout=5):
+    '''Check whether this URI is reachable, i.e. does it return a 200 OK?
+
+    This function returns True if a GET request to uri returns a 200 OK, and
+    False if that GET request returns any other response, or doesn't return
+    (i.e. times out).
+    '''
+    from requests.exceptions import ConnectionError
+    try:
+        r = requests.get(uri, timeout=timeout)
+        return r.status_code == requests.codes['ok']
+    except Exception as e:    # This is the correct syntax
+        return False
+    
+    
+    
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
+class Shortener(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # A GET request will either be for / (the root path) or for /some-name.
+        # Strip off the / and we have either empty string or a name.
+        name = unquote(self.path[1:])
+
+        if name:
+            if name in memory:
+                # 2. Send a 303 redirect to the long URI in memory[name].
+                #    Delete the following line.
+                self.send_response(303)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', memory[name]) #This will navigate to the original page
+                self.end_headers()
+            else:
+                # We don't know that name! Send a 404 error.
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write("I don't know '{}'.".format(name).encode())
+        else:
+            # Root path. Send the form.
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # List the known associations in the form.
+            known = "\n".join("{} : {}".format(key, memory[key])
+                              for key in sorted(memory.keys()))
+            self.wfile.write(form.format(known).encode())
+
+    def do_POST(self):
+        # Decode the form data.
+        length = int(self.headers.get('Content-length', 0))
+        body = self.rfile.read(length).decode()
+        params = parse_qs(body)
+
+        # Check that the user submitted the form fields.
+        if "longuri" not in params or "shortname" not in params:
+            # 3. Serve a 400 error with a useful message.
+            #    Delete the following line.
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Verify form fields, none can be empty.".encode())
+
+        longuri = params["longuri"][0]
+        shortname = params["shortname"][0]
+
+        if CheckURI(longuri):
+            # This URI is good!  Remember it under the specified name.
+            memory[shortname] = longuri
+
+            # 4. Serve a redirect to the root page (the form).
+            #    Delete the following line.
+            self.send_response(303)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/') #This will navigate to the original page
+            self.end_headers()
+        else:
+            # Didn't successfully fetch the long URI.
+
+            # 5. Send a 404 error with a useful message.
+            #    Delete the following line.
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("The URl couldn't be fetched this time.".encode())
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8001))   # Use PORT if it's there.
+    server_address = ('', port)
+    httpd = ThreadHTTPServer(server_address, Shortener)
+    httpd.serve_forever()
