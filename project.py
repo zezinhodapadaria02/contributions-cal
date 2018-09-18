d1b6c94ffd9c590cdda4c8616fd3ae45e1c42d25
Flaviano Marinho <flauberjp@gmail.com>
2018-09-17 21:03:13 -0300

    Extracting hash and summary

--- project.py
+++ project.py
@@ -104,7 +104,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
         data = simplejson.loads(self.data_string)
         print(str(data).encode())
-        print("New commit by: {}".format(data['push']['changes'][0]['new']['target']['author']['user']['username']))
+        print("Author: {}".format(data['push']['changes'][0]['new']['target']['author']['user']['username']))
+        print("Hash: {}".format(data['push']['changes'][0]['new']['target']['hash'][0:6]))
+        print("Summary: {}...".format(data['push']['changes'][0]['new']['target']['message'].rstrip()[0:6]))
         return "OK"
 
         if 'request' in self.path:
